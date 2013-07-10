08964dd5416bae31b5c09a67da63ba94678ea43c
steven <steven@steven-pc>
2013-07-10 18:03:25 +0100

    Removed config entries.

--- src/config/config.php
+++ src/config/config.php
@@ -1,6 +1,6 @@
 <?php
     return array(
-        'public_key'=>'6LejyOMSAAAAAGerCJOByT5BNkI3hnrIjTqurPQV',
-        'private_key'=>'6LejyOMSAAAAAAOI0QL9k_x3VBAI6VR0Lgkdp5V_'
+        'public_key'=>'',
+        'private_key'=>''
     );
 ?>
